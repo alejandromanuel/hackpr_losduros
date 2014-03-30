bc44bbd4afdfb5ddcc87cf2748a0b3f89b0e3446
Cptmemi <robertoell777@gmail.com>
2014-03-30 05:43:09 -0400

    Mano, ya es tarde.

--- hackathonproject/js/user_registration_script.js
+++ hackathonproject/js/user_registration_script.js
@@ -1,9 +1,9 @@
  $(document).ready(function(){
     Parse.initialize("yCot7DiqPN7MLHrmceO3kR5C8FzxPFLqynLBgc4A", "ffJ4m7XXPcVpQAJL3Jw212KbNn06Lp098Fl468Tx");
 
-     var name   = document.getElementById("InputName");
-     var email  = document.getElementById("InputEmail");
-     var phone  = document.getElementById("InputPhone");
+     // var name   = document.getElementById("InputName");
+     // var email  = document.getElementById("InputEmail");
+     // var phone  = document.getElementById("InputPhone");
 
 
     var Users = Parse.Object.extend("Users");
@@ -11,9 +11,9 @@
 
     $("#submit").click(function(){
 
-          users.set("InputName", InputName.value); 
+          users.set("Name", InputName.value); 
           users.set("Phone", InputPhone.value);
-          users.set("Email", InputEmail.value)
+          users.set("Email", InputEmail.value);
           users.save();
   }); 
 });
--- hackathonproject/js/voucher_registration_script.js
+++ hackathonproject/js/voucher_registration_script.js
@@ -1,18 +1,16 @@
-$(document).ready(function(){      
-  Parse.initialize("yCot7DiqPN7MLHrmceO3kR5C8FzxPFLqynLBgc4A", "ffJ4m7XXPcVpQAJL3Jw212KbNn06Lp098Fl468Tx")
+$(document).ready(function(){
+    Parse.initialize("yCot7DiqPN7MLHrmceO3kR5C8FzxPFLqynLBgc4A", "ffJ4m7XXPcVpQAJL3Jw212KbNn06Lp098Fl468Tx");
 
-     var title         = document.getElementById("InputVoucher");
-     var quantity      = document.getElementById("InputQuantity");
-     var enddate       = document.getElementById("InputEndDate");
-     var description   = document.getElementById("InputDescription");
-     var price         = document.getElementById("InputPrice");
-     var voucher_ID;
+     // var title         = document.getElementById("InputVoucher");
+     // var quantity      = document.getElementById("InputQuantity");
+     // var enddate       = document.getElementById("InputEndDate");
+     // var description   = document.getElementById("InputDescription");
+     // var price         = document.getElementById("InputPrice");
 
       var Vouchers = Parse.Object.extend("Vouchers");
-      var vouchers = new Vouchers();      
-      var query = new Parse.Query("Vouchers");
-      var Codes = Parse.Object.extend("Codes");
-      var codes = new Codes();
+       var vouchers = new Vouchers();      
+      // var Codes = Parse.Object.extend("Codes");
+      // var codes = new Codes();
       
 
 
@@ -23,17 +21,32 @@ $(document).ready(function(){
           vouchers.set("voucherEnddate", InputEndDate.value);
           vouchers.set("voucherDescription", InputDescription.value);
           vouchers.set("voucherPrice", InputPrice.value);
-          vouchers.save();
+          vouchers.save({
+              success: function(results){
+                alert("Funciono!");
+              },
+              error: function(model, error) {
+                // This will be called.
+                // error is an instance of Parse.Error with details about the error
+                if (error.code === Parse.Error.CONNECTION_FAILED) {
+                  alert("Uh oh, we couldn't even connect to the Parse Cloud!");
+                }     else if (error.code === Parse.Error.OBJECT_NOT_FOUND) {
+                  alert("Uh oh, we couldn't find the object!");
+                }
+          }
 
-           query.descending("createAt"); 
-           query.limit(1);
-           query.get({
-           success: function(voucher_ID) {
-           alert(voucher_ID);
-           codes.set("voucherCode",voucher_ID[0].id); 
-           codes.save();
-           }});
-           
 
-  }); 
+          // null,{
+          //   success: function(Object){
+          //       for(var i=0 ; i<InputQuantity.value; i++){
+          //         cupon.set("parentID", InputVoucher.value);
+          //         cupon.set("cuponID", cuponID[i]);
+          //         console.log(i);
+          //       }
+          //          cupon.save();
+          //     }});
+    
+      });
+  
+    });
 });
